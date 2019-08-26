49813223f5d9f5e9a067e9640b47be116711f473
SourceOverflow <oscarablinger@gmail.com>
2019-08-27 01:55:20 +0200

    feat(yaml-creator): Add yaml-creator page

---
+++ src/pages/CreatePage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="wrapper"></div>
+</template>
+
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator'
+
+  @Component({})
+  export default class CreatePage extends Vue {}
+</script>
+
+<style lang="stylus" scoped>
+  
+</style>
