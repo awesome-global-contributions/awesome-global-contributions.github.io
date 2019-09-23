816ff0a079dd7ce5929ef0a13a3c04b120db718a
Oscar Ablinger <dev.ablinger@gmail.com>
2019-09-23 16:21:20 +0200

    fix(setup): Fix indentation

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="app">
-    <ProjectList :projects="projects"/>
-  </div>
+    <div id="app">
+        <ProjectList :projects="projects"/>
+    </div>
 </template>
 
 <script lang="ts">
@@ -11,16 +11,16 @@ import Project from './dataobjects/Project'
 import { requestAllProjects } from './modules/RequestAllProjects'
 
 @Component({
-  components: {
-    ProjectList,
-  },
+    components: {
+        ProjectList,
+    },
 })
 export default class App extends Vue {
-  public projects: Project[] = []
+    public projects: Project[] = []
 
-  public mounted() {
-    this.projects = requestAllProjects()
-  }
+    public mounted() {
+        this.projects = requestAllProjects()
+    }
 }
 </script>
 
