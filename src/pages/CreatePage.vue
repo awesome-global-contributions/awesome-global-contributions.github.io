da3020ebbc22ef777409eb09b4c18fcd9e20144f
Oscar Ablinger <dev.ablinger@gmail.com>
2020-03-14 01:47:04 +0100

    fix(filter): linting

--- src/pages/CreatePage.vue
+++ src/pages/CreatePage.vue
@@ -5,15 +5,15 @@
 </template>
 
 <script lang="ts">
-  import { Component, Vue } from 'vue-property-decorator'
-  import ProjectForm from '../components/ProjectForm.vue'
+import { Component, Vue } from 'vue-property-decorator'
+import ProjectForm from '../components/ProjectForm.vue'
 
-  @Component({
-    components: {
-      ProjectForm
-    },
-  })
-  export default class CreatePage extends Vue {}
+@Component({
+  components: {
+    ProjectForm,
+  },
+})
+export default class CreatePage extends Vue {}
 </script>
 
 <style lang="stylus" scoped>
