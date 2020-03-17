041c3cc27435513c92ab86b1642bfe72ad42f066
Oscar Ablinger <dev.ablinger@gmail.com>
2020-03-17 18:49:29 +0100

    feat(yaml-creator): better feedback when copying to clipboard

--- src/components/YamlViewer.vue
+++ src/components/YamlViewer.vue
@@ -5,10 +5,10 @@
       :code="yaml"
     ></highlight-code>
     <el-button
-      type="primary"
-      :loading="loading"
+      :type="copyButtonType"
       v-show="copySupported && yaml != undefined"
-      @click="copyToClipboard">Copy to Clipboard</el-button>
+      :icon="copyIcon"
+      @click="copyToClipboard">{{ copyText }}</el-button>
   </div>
 </template>
 
@@ -18,25 +18,48 @@
 
   import { copyToClipboard } from '../modules/CopyToClipboard'
 
+  const COPY_TEXT = 'Copy to Clipboard'
+  const COPIED_TEXT = 'Successfully copied'
+
+  const COPY_BUTTON_TYPE_NORMAL = 'primary'
+  const COPY_BUTTON_TYPE_SUCCESS = 'success'
+
   @Component({
-      components: {
-        Input,
-      },
+    components: {
+      Input,
+    },
   })
   export default class YamlViewer extends Vue {
-      @Prop(String)
-      public yaml?: string
+    @Prop(String)
+    public yaml?: string
 
-      // only for visual feedback
-      private loading: boolean = false
-      private copySupported: boolean = document.queryCommandSupported('copy')
+    // only for visual feedback
+    private copyText: string = COPY_TEXT
+    private copyIcon: string = ''
+    private copyButtonType: string = COPY_BUTTON_TYPE_NORMAL
 
-      public copyToClipboard() {
-        // tslint:disable-next-line:triple-equals
-        if (this.yaml != undefined) {
-          copyToClipboard(this.yaml)
-        }
+    private copySupported: boolean = document.queryCommandSupported('copy')
+
+    public copyToClipboard() {
+      // tslint:disable-next-line:triple-equals
+      if (this.yaml != undefined) {
+        copyToClipboard(this.yaml)
+          .then(this.showCopiedVisualFeedback)
+          .catch(() => window.alert('Copy did not work. Most likely your browser doesn\'t support it'))
       }
+    }
+
+    private showCopiedVisualFeedback() {
+      this.copyText = COPIED_TEXT
+      this.copyIcon = 'el-icon-check'
+      this.copyButtonType = COPY_BUTTON_TYPE_SUCCESS
+
+      setTimeout(() => {
+        this.copyText = COPY_TEXT
+        this.copyIcon = ''
+        this.copyButtonType = COPY_BUTTON_TYPE_NORMAL
+      }, 2000)
+    }
   }
 </script>
 
