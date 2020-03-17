0ea06e309038b634e4dcac23f4e253ad0795722f
Oscar Ablinger <dev.ablinger@gmail.com>
2020-03-17 19:32:42 +0100

    feat(yaml-creator): add a bit of text

--- src/pages/CreatePage.vue
+++ src/pages/CreatePage.vue
@@ -1,6 +1,18 @@
 <template>
   <el-tabs type="border-card">
     <el-tab-pane label="Editor">
+      <p>
+        <a target="_blank" href="https://github.com/awesome-global-contributions/awesome-global-contributions/blob/master/CONTRIBUTING.md">Click here</a>
+        to see a detailed walk-through on how to add a project to this list.
+      </p>
+      <p>
+        It is recommended to first fill out the url of the project and then let us auto-fill
+        as much as possible through the button that will appear when you enter an URL.
+        Please do check whether those values are correct, though.
+      </p>
+      <p>
+        Thank you for adding a project to this list ♥        
+      </p>
       <ProjectForm
         @updateJson="updateJson"/>
     </el-tab-pane>
