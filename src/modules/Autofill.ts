c70d052da336f5b5a9c64519cdf303b83bb2e70d
Oscar Ablinger <dev.ablinger@gmail.com>
2020-03-18 21:13:25 +0100

    feat(yaml-creator): split ProjectAutoFillKeyValuePair into Success and Failed variant

--- src/modules/Autofill.ts
+++ src/modules/Autofill.ts
@@ -2,12 +2,20 @@ import ProjectJson from '@/dataobjects/ProjectJson'
 import { githubAutofill, githubRecognitionRegex } from './autofill/GithubAutofill'
 
 
-export interface ProjectAutoFillKeyValuePair<K extends keyof ProjectJson> {
+export interface ProjectAutoFillKeyValuePairFailed<K extends keyof ProjectJson> {
     key: K
-    failed: boolean
-    failReason?: string
-    value?: ProjectJson[K]
+    failed: true
+    failReason: string
 }
+
+export interface ProjectAutoFillKeyValuePairSuccess<K extends keyof ProjectJson> {
+    key: K
+    failed: false
+    value: ProjectJson[K]
+}
+
+export type ProjectAutoFillKeyValuePair<K extends keyof ProjectJson> =
+   ProjectAutoFillKeyValuePairFailed<K>|ProjectAutoFillKeyValuePairSuccess<K>
 
 export function autofill(url: string): Promise<Array<ProjectAutoFillKeyValuePair<keyof ProjectJson>>> {
     if (githubRecognitionRegex.test(url)) {
